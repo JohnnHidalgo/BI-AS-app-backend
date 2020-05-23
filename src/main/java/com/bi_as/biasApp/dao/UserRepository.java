256bdc362353a94260b4d7cf0d53416526a8234f
JohnnHidalgo <johnna.7h@gmail.com>
2020-05-23 19:37:55 -0400

    Create UserRepository Interface

---
+++ src/main/java/com/bi_as/biasApp/dao/UserRepository.java
@@ -0,0 +1,14 @@
+package com.bi_as.biasApp.dao;
+
+import com.bi_as.biasApp.domain.User;
+import org.springframework.data.repository.Repository;
+import java.util.List;
+
+public interface UserRepository extends Repository<User, Integer> {
+
+    List<User> findAll();
+    User findOne(int id);
+    User save(User p);
+    void delete(User p);
+
+}
