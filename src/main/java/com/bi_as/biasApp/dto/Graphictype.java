9f491865d7936b04b20ce6ae76d43677ab030526
JohnnHidalgo <johnna.7h@gmail.com>
2020-05-23 19:07:48 -0400

    Create Data Access Object for Ggraphictype

---
+++ src/main/java/com/bi_as/biasApp/dto/Graphictype.java
@@ -0,0 +1,49 @@
+package com.bi_as.biasApp.dto;
+
+public class Graphictype {
+    private Integer idGraphictype;
+    private String name;
+    private int active;
+
+    public Graphictype() {
+    }
+
+    public Graphictype(Integer idGraphictype, String name, int active) {
+        this.idGraphictype = idGraphictype;
+        this.name = name;
+        this.active = active;
+    }
+
+    public Integer getIdGraphictype() {
+        return idGraphictype;
+    }
+
+    public void setIdGraphictype(Integer idGraphictype) {
+        this.idGraphictype = idGraphictype;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Graphictype{" +
+                "idGraphictype=" + idGraphictype +
+                ", name='" + name + '\'' +
+                ", active=" + active +
+                '}';
+    }
+}
