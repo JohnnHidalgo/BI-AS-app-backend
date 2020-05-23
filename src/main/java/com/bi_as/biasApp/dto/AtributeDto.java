46c95ba72b67c86630932a2336cc51a283e2a98c
JohnnHidalgo <johnna.7h@gmail.com>
2020-05-23 19:24:09 -0400

    Addlasts  Dto

---
+++ src/main/java/com/bi_as/biasApp/dto/AtributeDto.java
@@ -0,0 +1,64 @@
+package com.bi_as.biasApp.dto;
+
+import com.bi_as.biasApp.domain.Graphic;
+
+import javax.persistence.*;
+
+public class AtributeDto {
+    private Integer idAtribute;
+    private String data;
+    private int active;
+    private Graphic graphicidgraphic;
+
+    public AtributeDto() {
+    }
+
+    public AtributeDto(Integer idAtribute, String data, int active, Graphic graphicidgraphic) {
+        this.idAtribute = idAtribute;
+        this.data = data;
+        this.active = active;
+        this.graphicidgraphic = graphicidgraphic;
+    }
+
+    public Integer getIdAtribute() {
+        return idAtribute;
+    }
+
+    public void setIdAtribute(Integer idAtribute) {
+        this.idAtribute = idAtribute;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public Graphic getGraphicidgraphic() {
+        return graphicidgraphic;
+    }
+
+    public void setGraphicidgraphic(Graphic graphicidgraphic) {
+        this.graphicidgraphic = graphicidgraphic;
+    }
+
+    @Override
+    public String toString() {
+        return "AtributeDto{" +
+                "idAtribute=" + idAtribute +
+                ", data='" + data + '\'' +
+                ", active=" + active +
+                ", graphicidgraphic=" + graphicidgraphic +
+                '}';
+    }
+}
--- src/main/java/com/bi_as/biasApp/dto/DashboardDto.java
+++ src/main/java/com/bi_as/biasApp/dto/DashboardDto.java
@@ -1,4 +1,82 @@
 package com.bi_as.biasApp.dto;
 
 public class DashboardDto {
+    private Integer idDashboard;
+    private String name;
+    private String txUser;
+    private String txHost;
+    private String txDate;
+    private int active;
+
+    public DashboardDto() {
+    }
+
+    public DashboardDto(Integer idDashboard, String name, String txUser, String txHost, String txDate, int active) {
+        this.idDashboard = idDashboard;
+        this.name = name;
+        this.txUser = txUser;
+        this.txHost = txHost;
+        this.txDate = txDate;
+        this.active = active;
+    }
+
+    public Integer getIdDashboard() {
+        return idDashboard;
+    }
+
+    public void setIdDashboard(Integer idDashboard) {
+        this.idDashboard = idDashboard;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTxUser() {
+        return txUser;
+    }
+
+    public void setTxUser(String txUser) {
+        this.txUser = txUser;
+    }
+
+    public String getTxHost() {
+        return txHost;
+    }
+
+    public void setTxHost(String txHost) {
+        this.txHost = txHost;
+    }
+
+    public String getTxDate() {
+        return txDate;
+    }
+
+    public void setTxDate(String txDate) {
+        this.txDate = txDate;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "DashboardDto{" +
+                "idDashboard=" + idDashboard +
+                ", name='" + name + '\'' +
+                ", txUser='" + txUser + '\'' +
+                ", txHost='" + txHost + '\'' +
+                ", txDate='" + txDate + '\'' +
+                ", active=" + active +
+                '}';
+    }
 }
